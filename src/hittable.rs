75d74e85d9289f0314c8fde1a6a5240d434e5c6c
elapuestojoe <kevin.islas.abud@gmail.com>
2022-09-18 13:34:49 -0700

    Adds hit table list class

--- src/hittable.rs
+++ src/hittable.rs
@@ -30,4 +30,41 @@ pub mod hittable {
     {
         fn hit(&self, ray: &Ray<T>, t_min: T, t_max: T, hit_record: &mut HitRecord<T>) -> bool;
     }
+
+    pub struct HittableList<T>
+    where
+        T: Float,
+    {
+        elements: Vec<Box<dyn Hittable<T>>>,
+    }
+
+    impl<T> HittableList<T>
+    where
+        T: Float,
+    {
+        pub fn new(elements: Vec<Box<dyn Hittable<T>>>) -> HittableList<T> {
+            HittableList { elements }
+        }
+    }
+
+    impl<T> Hittable<T> for HittableList<T>
+    where
+        T: Float,
+    {
+        fn hit(&self, ray: &Ray<T>, t_min: T, t_max: T, hit_record: &mut HitRecord<T>) -> bool {
+            let mut temporal_record = HitRecord::<T>::new();
+            let mut hit_anything = false;
+            let mut closest_so_far = t_max;
+            for element in self.elements.iter() {
+                if element.hit(ray, t_min, closest_so_far, &mut temporal_record) {
+                    hit_anything = true;
+                    closest_so_far = temporal_record.t;
+                }
+            }
+            if hit_anything {
+                *hit_record = temporal_record;
+            }
+            hit_anything
+        }
+    }
 }
